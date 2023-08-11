046d17f99b9ffa5f900cdecca5b821897ca79aa6
caferkosker <142021712+caferkosker@users.noreply.github.com>
2023-08-11 16:30:54 +0300

    GifMaker

---
+++ GifMaker/main.py
@@ -0,0 +1,7 @@
+import imageio
+
+filenames = ["team-pic1.png","team-pic2.png"]
+images = []
+for filename in filenames:
+    images.append(imageio.v2.imread(filename))
+    imageio.mimsave("team.gif", images, duration = 0.5)
